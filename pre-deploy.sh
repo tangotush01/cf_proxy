dc8724c4f98d05d22af0a5b8fcf3038ad4b99cd7
tangotush01 <tangotush@proton.me>
2026-07-29 16:51:14 +0530

    Update pre-deploy.sh

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing headless browser system dependencies..."
-apt-get update
-apt-get install -y --no-install-recommends \
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
     libgtk-3-0 libdbus-glib-1-2 libxt6 libasound2 \
     libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 \
     libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 \
